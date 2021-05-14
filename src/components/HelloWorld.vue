3b2baac50161fd2324473b6c53814786a98fd336
thinkphp <mergesortv@gmail.com>
2021-05-14 09:32:10 +0300

    first commit app chelner

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+
+p {{ greeting }} to the App Chelner!
+
+otherComponent
+
+<div>
+  <h2>Count: {{ counter }}</h2>
+  <button @click="increaseClick()">Plus</button>
+  <button @click="decreaseClick()">Minus</button>
+</div>
+
+
+<p class='footer'>Created by <a href='http://adrianstatescu.ch'>Adrian Statescu</a> using yarn create @vitejs/app app</p>
+
+</template>
+
+<script>
+
+import otherComponent from './otherComponent.vue'
+
+export default {
+
+  components: {
+  
+   otherComponent
+  
+  },
+
+  data() {
+    return {
+      greeting: 'Welcome ',
+      counter: 0
+    }
+  },
+  
+  methods: {
+  
+    increaseClick () {
+          
+        this.counter = this.counter + 1 
+    },
+         
+    decreaseClick () {
+         
+       this.counter = this.counter - 1  
+         
+    },
+         
+  }
+}
+</script>
+
+<style lang="stylus">
+p
+  font-size 2em
+  text-align center  
+  font-family: verdana
+p.footer
+  font-size 1em
+  text-align left
+  font-family: verdana
+  
+</style>
+
---
+++ src/components/otherComponent.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+p I will be billionaire in 2021 because I am an IronMan!
+</template>
+
+<style lang="stylus" scoped>
+p
+  font-family sans-serif
+  font-size 1em
+  border 3px solid #3eaf7c
+  width 65%
+  padding 15px
+  margin 0 auto
+  background lightyellow
+  text-align center
+</style>
